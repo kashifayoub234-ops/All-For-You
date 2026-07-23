95f4418f236853064b966555e7d808a46ab11026
kashifayoub234-ops <kashifayoub234@gmail.com>
2026-07-24 00:12:15 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -85,3 +85,22 @@ function clearCart(){
 
 
 showCart();
+function searchProduct(){
+
+    let input = document.getElementById("searchBox").value.toLowerCase();
+
+    let products = document.querySelectorAll(".product-card");
+
+    products.forEach(product => {
+
+        let name = product.innerText.toLowerCase();
+
+        if(name.includes(input)){
+            product.style.display = "block";
+        }else{
+            product.style.display = "none";
+        }
+
+    });
+
+}
