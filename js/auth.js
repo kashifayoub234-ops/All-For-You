63e60e572efcb93b0522e2f869a0274f3f939776
kashifayoub234-ops <kashifayoub234@gmail.com>
2026-07-28 20:12:33 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,26 @@
+import { auth } from "./firebase-config.js";
 
+import {
+    onAuthStateChanged,
+    signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+
+    if (user) {
+        console.log("Logged In:", user.email);
+    } else {
+        console.log("No User Logged In");
+    }
+
+});
+
+window.logoutUser = async function () {
+
+    await signOut(auth);
+
+    alert("Logged out successfully.");
+
+    window.location.href = "login.html";
+
+};
